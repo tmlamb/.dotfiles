503b8d25c7a4337571b4ddccd8a2c3834e81cdee
Tom Lamb <tmlamb@gmail.com>
2023-02-11 18:56:49 -0600

    changed tab width

--- nvim/.config/nvim/lua/leek/remap.lua
+++ nvim/.config/nvim/lua/leek/remap.lua
@@ -10,16 +10,16 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- greatest remap ever
+-- Paste from system clipboard
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
--- next greatest remap ever : asbjornHaland
+-- Credit: asbjornHaland
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
--- This is going to get me cancelled
+-- Todo
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
