7672d2a4d7f9028dd38b94045434cf30a5198e1f
Tom Lamb <tmlamb@gmail.com>
2023-09-24 11:04:13 -0500

    lsp-zero deprecations

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -2,60 +2,57 @@ local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
 
-lsp.ensure_installed({
-  'tsserver',
-  'eslint',
-  'lua_ls',
-  'rust_analyzer',
-  'angularls',
-  'cssls',
-})
+require('mason').setup({})
 
--- Fix Undefined global 'vim'
-lsp.configure('lua_ls', {
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { 'vim' }
-      }
-    }
-  }
-})
-
-lsp.configure('tailwindcss', {
-  settings = {
-    tailwindCSS = {
-      experimental = {
-        classRegex = {
-          "tw`([^`]*)",
-          "tw=\"([^\"]*)",
-          "tw={\"([^\"}]*)",
-          "tw\\.\\w+`([^`]*)",
-          "tw\\(.*?\\)`([^`]*)",
-          "tw.style\\(([^)]*)\\)",
+require('mason-lspconfig').setup({
+  ensure_installed = {
+    'tsserver',
+    'eslint',
+    'lua_ls',
+    'rust_analyzer',
+    'angularls',
+    'cssls',
+  },
+  handlers = {
+    lsp.default_setup,
+    tailwindcss = function()
+      require('lspconfig').tailwindcss.setup({
+        settings = {
+          tailwindCSS = {
+            experimental = {
+              classRegex = {
+                "tw`([^`]*)",
+                "tw=\"([^\"]*)",
+                "tw={\"([^\"}]*)",
+                "tw\\.\\w+`([^`]*)",
+                "tw\\(.*?\\)`([^`]*)",
+                "tw.style\\(([^)]*)\\)",
+              }
+            }
+          }
         }
-      }
-    }
-  }
+      })
+    end,
+  },
 })
 
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
-local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
+  })
 })
+
 
 -- disable completion with tab
 -- this helps with copilot setup
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
+-- cmp_mappings['<Tab>'] = nil
+-- cmp_mappings['<S-Tab>'] = nil
 
-lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
-})
 
 lsp.set_preferences({
   suggest_lsp_servers = false,
