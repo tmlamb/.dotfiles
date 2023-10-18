77600d2e0368f60d14e19050f4c9fb2e66a16b7e
Tom Lamb <tmlamb@gmail.com>
2023-10-17 23:37:35 -0500

    wip

--- nvim/.config/nvim/after/plugin/neoformat.lua
+++ nvim/.config/nvim/after/plugin/neoformat.lua
@@ -2,7 +2,7 @@ local autocmd = vim.api.nvim_create_autocmd
 
 vim.g.neoformat_try_node_exe = 1
 
-autocmd('BufWritePre,TextChanged,InsertLeave', {
+autocmd({'BufWritePre','TextChanged','InsertLeave'}, {
   pattern = {'*.js', '*.ts', '*.jsx', '*.tsx', '*.css', '*.scss', '*.html', '*.json', '*.yaml', '*.yml', '*.md'},
   command = ':Neoformat'
 })
