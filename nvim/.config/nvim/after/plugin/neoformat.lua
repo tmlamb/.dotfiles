a5ec9b01c4084369546bcdd1deee34504ff9e4fe
Tom Lamb <tmlamb@gmail.com>
2023-10-29 22:57:08 -0500

    hurl, fixed autocomplete, and fixed neoformat

--- nvim/.config/nvim/after/plugin/neoformat.lua
+++ nvim/.config/nvim/after/plugin/neoformat.lua
@@ -1,8 +1,9 @@
 local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
 
 vim.g.neoformat_try_node_exe = 1
-
-autocmd({'BufWritePre','TextChanged','InsertLeave'}, {
+-- ,'TextChanged','InsertLeave'
+autocmd({'BufWritePre'}, {
   pattern = {'*.js', '*.ts', '*.jsx', '*.tsx', '*.css', '*.scss', '*.html', '*.json', '*.yaml', '*.yml', '*.md'},
   command = ':Neoformat'
 })
--- nvim/.config/nvim/lua/leek/packer.lua
+++ nvim/.config/nvim/lua/leek/packer.lua
@@ -13,7 +13,7 @@ return require('packer').startup(function(use)
   }
 
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    'nvim-telescope/telescope.nvim', tag = '0.1.3',
     -- or                            , branch = '0.1.x',
     requires = { { 'nvim-lua/plenary.nvim' } }
   }
@@ -58,4 +58,6 @@ return require('packer').startup(function(use)
   use("sbdchd/neoformat")
 
   use("norcalli/nvim-colorizer.lua")
+
+  use('pfeiferj/nvim-hurl')
 end)
